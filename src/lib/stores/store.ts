d683725a0d4f84813821156717334cf97c9f6972
Jonas Pöppelmann <jonas.poeppelmann@gmail.com>
2022-02-24 15:24:17 +0100

    Save language in local storage

--- src/lib/stores/store.ts
+++ src/lib/stores/store.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
 export enum Language {
@@ -5,7 +6,15 @@ export enum Language {
 	EN = 'en'
 }
 
-export const language = writable(Language.EN);
+let storedLanguage: string = Language.EN;
+if (browser) {
+	storedLanguage = localStorage.getItem('language');
+}
+export const language = writable(storedLanguage || Language.EN);
+
+if (browser) {
+	language.subscribe((value) => localStorage.setItem('language', value));
+}
 
 export const toggleLanguage = () => {
 	language.update((language) => (language === Language.DE ? Language.EN : Language.DE));
