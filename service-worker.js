bec692488493b40c1752125807d02dc570ab39f3
qelix <jonas.poeppelmann@gmail.com>
2022-05-24 14:04:40 +0000

    Deploying to gh-pages from @ qelix/tfl-tierlist@bf6fe1c27c1a7452413d848aeff8b4f1652a6fcb 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const timestamp = 1653400845990;
+const timestamp = 1653401063625;
 const build = [
-  "/_app/start-28353989.js",
+  "/_app/start-d8a82981.js",
   "/_app/pages/__layout.svelte-b238adbd.js",
   "/_app/assets/pages/__layout.svelte-b2693581.css",
   "/_app/error.svelte-f2d62e4f.js",
