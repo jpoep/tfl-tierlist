13e069ee39083470b7cfca362117180b56bf58ac
qelix <jonas.poeppelmann@gmail.com>
2022-06-08 20:39:59 +0000

    Deploying to gh-pages from @ qelix/tfl-tierlist@e04a7eb5ff1c3fb64157d30d98dbee6ac8835c62 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const build = [
-  "/_app/immutable/start-10f802c6.js",
+  "/_app/immutable/start-0008cf92.js",
   "/_app/immutable/pages/__layout.svelte-e1feb261.js",
   "/_app/immutable/assets/pages/__layout.svelte-a1942712.css",
   "/_app/immutable/assets/titillium-web-latin-ext-400-normal-05e41516.woff2",
@@ -9,8 +9,8 @@ const build = [
   "/_app/immutable/assets/titillium-web-all-700-normal-65d21c1b.woff",
   "/_app/immutable/assets/titillium-web-latin-700-normal-d5c1172f.woff2",
   "/_app/immutable/error.svelte-69190d4e.js",
-  "/_app/immutable/pages/index.svelte-8e2d24c0.js",
-  "/_app/immutable/assets/pages/index.svelte-e96a99d9.css",
+  "/_app/immutable/pages/index.svelte-d73e98e4.js",
+  "/_app/immutable/assets/pages/index.svelte-d573c15b.css",
   "/_app/immutable/chunks/index-a64115f5.js",
   "/_app/immutable/chunks/index-6bee92c9.js",
   "/_app/immutable/chunks/paths-396f020f.js",
@@ -77,7 +77,7 @@ const files = [
   "/pokeball-logo.svg",
   "/till.png"
 ];
-const version = "1654719464110";
+const version = "1654720779016";
 const worker = self;
 const FILES = `cache${version}`;
 const to_cache = build.concat(files.filter((it) => !it.endsWith(".nojekyll")));
