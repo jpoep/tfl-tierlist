478c85c48a10dae78fe94572091f3e8421b7550e
qelix <jonas.poeppelmann@gmail.com>
2022-07-16 14:40:35 +0000

    Deploying to gh-pages from @ qelix/tfl-tierlist@9f0295dfbb1913aeacb529ac247568e3dfe0c5ef 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const build = [
-  "/_app/immutable/start-acafe1c3.js",
+  "/_app/immutable/start-df9718ed.js",
   "/_app/immutable/pages/__layout.svelte-4064d19a.js",
   "/_app/immutable/assets/pages/__layout.svelte-c77a3056.css",
   "/_app/immutable/assets/titillium-web-latin-ext-400-normal-05e41516.woff2",
@@ -116,7 +116,7 @@ const files = [
   "/sounds/till.mp3",
   "/till.png"
 ];
-const version = "1657981765236";
+const version = "1657982417813";
 const worker = self;
 const FILES = `cache${version}`;
 const to_cache = build.concat(files.filter((it) => !it.endsWith(".nojekyll")));
